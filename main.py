8bae112a16c414fbe8ca15dd523c27615d47119c
devrockingout <econt037@uottawa.ca>
2020-02-08 19:36:08 -0500

    made a separate module for the server

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from modules.user_interface.UserInterface import UserInterface
+from modules.server.server import start_web_server
 
-class Server(BaseHTTPRequestHandler):
+def main():
+    start_web_server() #comment this line to test other modules without lunching web server
 
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/modules/user_interface/user_interface.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-    def do_POST(self):
-        # maybe there is a better way to get POST without libraries
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        params = post_data.decode('utf-8').split("&")
-
-        POST = {}
-        for x in params:
-            parts = x.split("=")
-            key = parts[0]
-            value = parts[1]
-            POST.update({key : value})
-        
-        UI = UserInterface(POST["query"])
-        docs_collection = UI.test() # later will return the collection of docs
-
-        self.send_response(200)
-        #self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(bytes(docs_collection, 'utf-8'))
-        #self.wfile.write(json_str.encode(encoding='utf_8'))
-
-
-httpd = HTTPServer(('localhost', 8080), Server)
-httpd.serve_forever()
+main()
