3816d3c703ac6482dee5dce2bc5e753f812c0c6c
devrockingout <econt037@uottawa.ca>
2020-02-08 22:29:13 -0500

    pre-processing module v1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from modules.server.server import start_web_server
+from modules.corpus.PreProcessing import PreProcessing
 
 def main():
-    start_web_server() #comment this line to test other modules without lunching web server
+    #start_web_server() #comment this line to test other modules without lunching web server
+    p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
+    p.generate_corpus()
 
 main()
