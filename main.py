b0d1d49b9d3b63f14480936cced66d9e1756d440
devrockingout <econt037@uottawa.ca>
2020-02-09 17:48:00 -0500

    Access module v1

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from modules.server.server import start_web_server
 from modules.corpus.PreProcessing import PreProcessing
+from modules.corpus.Access import Access
 
 def main():
     #start_web_server() #comment this line to test other modules without lunching web server
     p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
     p.generate_corpus()
+
+    corpus = Access("data/uo_courses.json")
+    print(corpus.get_doc(146))
+    print(corpus.get_docs([1,5,70,135,345]))
 
 main()
