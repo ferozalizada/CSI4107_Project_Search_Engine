528fb701e5f464d6ab6bb76c3dc18b008b722603
devrockingout <econt037@uottawa.ca>
2020-02-11 21:40:53 -0500

    user interface module v1

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from modules.corpus.PreProcessing import PreProcessing
 from modules.corpus.Access import Access
 
 def main():
-    #start_web_server() #comment this line to test other modules without lunching web server
-    p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
-    p.generate_corpus()
+    start_web_server() #comment this line to test other modules without lunching web server
+    #p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
+    #p.generate_corpus()
 
-    corpus = Access("data/uo_courses.json")
-    print(corpus.get_doc(146))
-    print(corpus.get_docs([1,5,70,135,345]))
+    #corpus = Access("data/uo_courses.json")
+    #print(corpus.get_doc(146))
+    #print(corpus.get_docs([1,5,70,135,345]))
 
 main()
