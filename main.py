bb5ef1ea7166ceb95f7598ac3543498a2460bdd0
devrockingout <econt037@uottawa.ca>
2020-02-15 13:14:44 -0500

    Boolean retrieval model v1

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from modules.server.Server import start_web_server
 from modules.corpus.PreProcessing import PreProcessing
 # from modules.corpus.Access import Access
+from modules.boolean_retrieval.BooleanModel import BooleanModel
 
 
 def main():
-    start_web_server()  # comment this line to test other modules without lunching web server
-    p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
-    p.generate_corpus()
+    q = BooleanModel('printer AND (laser OR ink)')
+    
+    print(q.search())
+    #print(q.infixToPostfix('(7 AND 8) OR (3 AND 2)'))
+    #print(q.infixToPostfix('(7 + 8) / (3 + 2)'))
+    #print(q.postfixEval('7 8 + 3 2 + /'))
+    # start_web_server()  # comment this line to test other modules without lunching web server
+    #p = PreProcessing("data/original_collection.html", "data/uo_courses.json")
+    #p.generate_corpus()
     # p.fun()
 
     #corpus = Access("data/uo_courses.json")
--- modules/boolean_retrieval/BooleanModel.py
+++ modules/boolean_retrieval/BooleanModel.py
@@ -1,6 +1,59 @@
 ## Name in diagram: boolean retrieval model
+from modules.boolean_retrieval.QueryPreProcessing import QueryPreProcessing
+import random 
+
 class BooleanModel:
 
-    def __init__(self, arg1, arg2):
-        self.arg1 = arg1
-        self.arg2 = arg2
+    def __init__(self, query):
+        self.query = query
+
+    def search(self):
+        prec = {}
+        prec["AND"] = 3
+        prec["OR"] = 4
+        prec["AND_NOT"] = 2
+        prec["("] = 1
+
+        q = QueryPreProcessing()
+        postfix_expr = q.infixToPostfix(prec, self.query)
+        result = q.postfixEval(self.__searchInIndex, prec, postfix_expr)
+        self.__tempIndex()
+        return result
+
+    def __searchInIndex(self, op, op1, op2, docs):
+
+        if(op == "AND"):
+            # op1 AND op2 must be in index
+            docs.append(op1) # append docs retrieved in inverted index
+        elif(op == "OR"):
+            # op1 OR op2 must be in index
+            docs.append(op2) # append docs retrieved in inverted index
+        elif(op == "AND_NOT"):
+            # op1 must be in index but not op2
+            docs.append(op1) # append docs retrieved in inverted index
+
+        return docs
+
+    def __tempIndex(self):
+        dictionary = ['printer', 'ink', 'laser', 'printer', 'ink', 'laser', 'printer', 'ink', 'laser','printer', 'ink', 'laser'] # terms
+        terms_docsID = {}
+
+        # build a list of term/docID pairs
+        for term in dictionary:
+            docID = random.randint(1,500)
+            if(term in terms_docsID):
+                terms_docsID[term].append(docID)
+            else:
+                terms_docsID[term] = [docID]
+        
+        # sort docsID
+        for term in terms_docsID:
+            terms_docsID[term].sort()
+        
+        print(terms_docsID)
+        
+
+
+    #https://www.geeksforgeeks.org/python-add-new-keys-to-a-dictionary/
+    #https://stackoverflow.com/questions/1602934/check-if-a-given-key-already-exists-in-a-dictionary
+    #https://www.geeksforgeeks.org/python-list-sort/
