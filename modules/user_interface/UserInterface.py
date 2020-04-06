5e4580d9a9a3d74c081b784890a4a5617b3e7d95
devrockingout <econt037@uottawa.ca>
2020-04-05 20:01:26 -0400

    vsm is now implemented on this branch

--- modules/user_interface/UserInterface.py
+++ modules/user_interface/UserInterface.py
@@ -1,13 +1,10 @@
-## Name in diagram: user interface
+# Name in diagram: user interface
 from modules.boolean_retrieval.BooleanModel import BooleanModel
 from modules.vector_space.VectorSpaceModel import VectorSpaceModel
 from modules.corpus.Access import Access
 from modules.text_processing.Stopword import *
 from modules.text_processing.Stemmer import *
 from modules.text_processing.Normalizer import *
-
-# from modules.vector_space.VectorSpaceModel import VectorSpaceModel
-from modules.corpus.Access import Access ######## THIS IS ONLY FOR TEST ########
 
 
 class UserInterface:
@@ -22,42 +19,58 @@ class UserInterface:
 
   def getDocs(self):
     docs = []
+    scores = []
 
     if(self.model == "Boolean"):
-      self.query = self.__cleanQuery()
-      q = BooleanModel(self.query)
-      
-      docs = q.search()
-      docs = list( dict.fromkeys(docs) )
-      
+        self.query = self.__cleanQuery()
+        q = BooleanModel(self.query)
+
+        # print(q)
+        docs = q.search()
+        docs = list(dict.fromkeys(docs))
+
     elif(self.model == "VSM"):
-      docs = []
+        docs = []
+        self.query = self.__cleanQuery()
+        vsm = VectorSpaceModel().retrieve(self.query)
+        scores = [i[1] for i in vsm]
+        docs = [i[0] for i in vsm]
 
     corpus = Access("data/uo_courses.json")
+
     if len(docs) > 0:
-      return corpus.get_docs(docs)
+        json_docs = []
+        for i, item in enumerate(corpus.get_docs(docs)):
+            if self.model == "VSM":
+                item['score'] = scores[i]
+                json_docs.append(item)
+            else:
+                item['score'] = 1
+                json_docs.append(item)
+        print(json_docs)
+        return json_docs
     else:
-      return []
+        return []
 
   def __cleanQuery(self):
     q = self.query.split()
-    
+
     for i in range(len(q)):
-      if q[i] not in ["AND", "AND_NOT", "OR"]:
-        # lower()
-        q[i] = q[i].lower()
-        # Stemming
-        q[i] = self.__stemmer.stem_word(q[i])
-        # Normalization
-        q[i] = self.__normalizer.normalize(q[i])
+        if q[i] not in ["AND", "AND_NOT", "OR"]:
+            # lower()
+            q[i] = q[i].lower()
+            # Stemming
+            q[i] = self.__stemmer.stem_word(q[i])
+            # Normalization
+            q[i] = self.__normalizer.normalize(q[i])
 
     # StopWords removal
     self.__stopword = Stopword().get_stop_words()
 
     for word in self.__stopword:
-      q = [e for e in q if e not in word or e in ["AND", "AND_NOT", "OR"]]
+        q = [e for e in q if e not in word or e in ["AND", "AND_NOT", "OR"]]
 
     return ' '.join(q)
 
-#https://www.sqlite.org/download.html
-#https://stackoverflow.com/questions/55190327/nltk-importerror-dll-load-failed-the-specified-module-could-not-be-found
+# https://www.sqlite.org/download.html
+# https://stackoverflow.com/questions/55190327/nltk-importerror-dll-load-failed-the-specified-module-could-not-be-found
